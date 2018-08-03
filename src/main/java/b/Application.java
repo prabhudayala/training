8f1add2ecd6ee3b84f446ba236c4e2c454aadb3a
Your Name <you@example.com>
2018-08-03 16:54:56 +0530

    wdqwdqwdqwd0

--- src/main/java/b/Application.java
+++ src/main/java/b/Application.java
@@ -1,10 +1,7 @@
 package b;
 
-import java.sql.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -16,26 +13,31 @@ import org.springframework.web.client.RestTemplate;
 @EnableCaching
 public class Application extends SpringBootServletInitializer {
 
+	// public class Application {
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
-		
-		RestTemplate restTemplate = new RestTemplate(); 
+
+		RestTemplate restTemplate = new RestTemplate();
 		System.out.println(System.currentTimeMillis());
-		/*for(int i =0;i<10000;i++){
-		Quote quote = restTemplate.getForObject(
-				 "http://localhost:8080/EmpMgt/getAllEmpDetails", Quote.class);}
-		System.out.println(System.currentTimeMillis());*/
-		
+		/*
+		 * for(int i =0;i<10000;i++){ Quote quote = restTemplate.getForObject(
+		 * "http://localhost:8080/EmpMgt/getAllEmpDetails", Quote.class);}
+		 * System.out.println(System.currentTimeMillis());
+		 */
+
 	}
+
 	@Bean
-	public CacheManager cacheManager(){
+	public CacheManager cacheManager() {
 		ConcurrentMapCacheManager cm = new ConcurrentMapCacheManager("employee");
 		return cm;
-		
+
 	}
-	/*@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }*/
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder application) { return
+	 * application.sources(Application.class); }
+	 */
 }
